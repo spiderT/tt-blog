15b6870734bca8fded4c0be7bed26a0376374992
tangting <tangting@lianjia.com>
2017-10-20 15:21:21 +0800

    react-redux-todomvcDemo

--- react/shopping-cart/src/api/shop.js
+++
@@ -1,8 +0,0 @@
-import _products from './products.json'
-
-const TIMEOUT = 100
-
-export default {
-    getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
-    buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
-}
